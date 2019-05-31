4e6198537e188e726287bda41e8891dae0e6d062
Dhanush S <dhanushs@fredriks_macbo.corp.capgemini.com>
2019-05-31 12:59:36 +0530

    Initial Commit

---
+++ Example17/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example17
+//
+//  Created by Dhanush S on 31/05/19.
+//  Copyright © 2019 Dhanush S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
